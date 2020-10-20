df03d02eaf2a702cb7224e54dc65928cc4c09e6b
bsrodrigue <bsrodrigue@gmail.com>
2020-10-20 19:32:17 +0000

    fuckoff

--- stallion_novels/settings.py
+++ stallion_novels/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            BASE_DIR / 'stallion_novels/templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'stallion_novels/static'
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
